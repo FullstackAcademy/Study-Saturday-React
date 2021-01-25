573ef097487190dd0a80f3a1946d791cadba29e6
Mary Gordanier <mcgordanier@gmail.com>
2021-01-25 18:15:33 -0500

    refactor(app, seed, routes): pull improved syntax from cycle-1

--- routes/test.js
+++ routes/test.js
@@ -2,45 +2,74 @@ const router = require('express').Router();
 const Test = require('../db/models/tests');
 const Student = require('../db/models/students');
 
+// GET /api/tests
 router.get('/', async (req, res, next) => {
   try {
     const tests = await Test.findAll();
-    res.send(tests);
-  } catch (err) {
-    next(err);
+    res.json(tests);
+  } catch (error) {
+    next(error);
   }
 });
 
+// GET /api/tests/passing
+router.get('/passing', async (req, res, next) => {
+  try {
+    const passingTests = await Test.getPassing();
+    res.json(passingTests);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /api/tests/subjects/:subject
+router.get('/subjects/:subject', async (req, res, next) => {
+  try {
+    const testsBySubject = await Test.findAll({
+      where: {
+        subject: req.params.subject,
+      },
+    });
+    res.json(testsBySubject);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /api/tests/:id
 router.get('/:id', async (req, res, next) => {
   try {
-    let test = await Test.findByPk(req.params.id);
-    if (test) {
-      res.send(test);
-    } else {
-      res.status(404).send('Test not found');
-    }
-  } catch (err) {
-    next(err);
+    const test = await Test.findByPk(req.params.id);
+    res.json(test);
+  } catch (error) {
+    next(error);
   }
 });
 
-router.post('/student/:studentId', async (req, res, next) => {
+// POST /api/tests/students/:studentId
+router.post('/students/:studentId', async (req, res, next) => {
   try {
-    let student = await Student.findByPk(req.params.studentId);
+    const student = await Student.findByPk(req.params.studentId);
     let test = await Test.create(req.body);
-    let studentTest = await test.setStudent(student);
-    res.status(201).send(studentTest);
-  } catch (err) {
-    next(err);
+    test = await test.setStudent(student);
+    res.status(201).json(test);
+  } catch (error) {
+    next(error);
   }
 });
 
+// DELETE /api/tests/:id
 router.delete('/:id', async (req, res, next) => {
   try {
-    await Test.destroy({ where: { id: req.params.id } });
-    res.status(204).send();
-  } catch (err) {
-    next(err);
+    const test = await Test.findByPk(req.params.id);
+    if (test) {
+      await test.destroy();
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
--- seed.js
+++ seed.js
@@ -3,126 +3,136 @@ const Student = require('./db/models/students');
 const Test = require('./db/models/tests');
 const faker = require('faker');
 
-db.sync({ force: true })
-  .then(() => {
-    return Promise.all([
-      Student.create({
-        firstName: 'Jordan',
-        lastName: 'Walke',
-        email: 'jw@react.com',
-      }),
-      Student.create({
-        firstName: 'Dan',
-        lastName: 'Abramov',
-        email: 'da@react.com',
-      }),
-      Student.create({
-        firstName: 'James',
-        lastName: 'Dean',
-        email: 'jd@eastofeden.com',
-      }),
-      Student.create({
-        firstName: 'John',
-        lastName: 'Wayne',
-        email: 'jw@truegrit.com',
-      }),
-      Student.create({
-        firstName: 'Cary',
-        lastName: 'Grant',
-        email: 'cg@ithappened1nite.com',
-      }),
-    ]);
-  })
-  .then(() => {
-    return Promise.all([
-      Test.create({
-        subject: 'Computer Science',
-        grade: 45,
-        studentId: 1,
-      }),
-      Test.create({
-        subject: 'Art History',
-        grade: 67,
-        studentId: 2,
-      }),
-      Test.create({
-        subject: 'Business',
-        grade: 29,
-        studentId: 3,
-      }),
-      Test.create({
-        subject: 'History',
-        grade: 88,
-        studentId: 4,
-      }),
-      Test.create({
-        subject: 'English',
-        grade: 90,
-        studentId: 5,
-      }),
-      Test.create({
-        subject: 'Art',
-        grade: 60,
-        studentId: 1,
-      }),
-      Test.create({
-        subject: 'Social Sciences',
-        grade: 70,
-        studentId: 2,
-      }),
-      Test.create({
-        subject: 'Math',
-        grade: 88,
-        studentId: 2,
-      }),
-      Test.create({
-        subject: 'Birds',
-        grade: 50,
-        studentId: 3,
-      }),
-      Test.create({
-        subject: 'Art',
-        grade: 66,
-        studentId: 4,
-      }),
-      Test.create({
-        subject: 'Anything',
-        grade: 20,
-        studentId: 5,
-      }),
-      Test.create({
-        subject: faker.lorem.word(),
-        grade: 45,
-        studentId: 1,
-      }),
-      Test.create({
-        subject: faker.lorem.word(),
-        grade: 67,
-        studentId: 2,
-      }),
-      Test.create({
-        subject: faker.lorem.word(),
-        grade: 29,
-        studentId: 3,
-      }),
-      Test.create({
-        subject: faker.lorem.word(),
-        grade: 88,
-        studentId: 4,
-      }),
-      Test.create({
-        subject: faker.lorem.word(),
-        grade: 90,
-        studentId: 5,
-      }),
-    ]);
-  })
-  .then(() => {
-    console.log('yay we did it!');
+const students = [
+  {
+    firstName: 'Jordan',
+    lastName: 'Walke',
+    email: 'jw@react.com',
+  },
+  {
+    firstName: 'Dan',
+    lastName: 'Abramov',
+    email: 'da@react.com',
+  },
+  {
+    firstName: 'James',
+    lastName: 'Dean',
+    email: 'jd@eastofeden.com',
+  },
+  {
+    firstName: 'John',
+    lastName: 'Wayne',
+    email: 'jw@truegrit.com',
+  },
+  {
+    firstName: 'Cary',
+    lastName: 'Grant',
+    email: 'cg@ithappened1nite.com',
+  },
+];
+
+const tests = [
+  {
+    subject: 'Computer Science',
+    grade: 45,
+    studentId: 1,
+  },
+  {
+    subject: 'Art History',
+    grade: 67,
+    studentId: 2,
+  },
+  {
+    subject: 'Business',
+    grade: 29,
+    studentId: 3,
+  },
+  {
+    subject: 'History',
+    grade: 88,
+    studentId: 4,
+  },
+  {
+    subject: 'English',
+    grade: 90,
+    studentId: 5,
+  },
+  {
+    subject: 'Art',
+    grade: 60,
+    studentId: 1,
+  },
+  {
+    subject: 'Social Sciences',
+    grade: 70,
+    studentId: 2,
+  },
+  {
+    subject: 'Math',
+    grade: 88,
+    studentId: 2,
+  },
+  {
+    subject: 'Birds',
+    grade: 50,
+    studentId: 3,
+  },
+  {
+    subject: 'Art',
+    grade: 66,
+    studentId: 4,
+  },
+  {
+    subject: 'Anything',
+    grade: 20,
+    studentId: 5,
+  },
+  {
+    subject: faker.lorem.word(),
+    grade: 45,
+    studentId: 1,
+  },
+  {
+    subject: faker.lorem.word(),
+    grade: 67,
+    studentId: 2,
+  },
+  {
+    subject: faker.lorem.word(),
+    grade: 29,
+    studentId: 3,
+  },
+  {
+    subject: faker.lorem.word(),
+    grade: 88,
+    studentId: 4,
+  },
+  {
+    subject: faker.lorem.word(),
+    grade: 90,
+    studentId: 5,
+  },
+];
+
+const seed = async () => {
+  try {
+    await db.sync({ force: true });
+
+    // Students
+    await Student.bulkCreate(students);
+    console.log('Seeded students');
+
+    // Tests
+    await Test.bulkCreate(tests);
+    console.log('Seeded tests');
+
+    console.log('Database sucessfully seeded');
     db.close();
-  })
-  .catch(err => {
-    console.log('an error ack');
-    console.log(err.stack);
+  } catch (error) {
+    console.log('Error seeding database');
+    console.log(error.stack);
     db.close();
-  });
+  }
+};
+
+seed();
