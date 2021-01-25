711373a84a890ee05761440a29cfc31275ed2184
Maddie Higgins <maddiehiggins9@gmail.com>
2021-01-25 17:22:05 -0600

    merge dependency and syntax updates

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const Student = require('./routes/student')
-const Test = require('./routes/test')
-const morgan = require('morgan')
-const db = require('./db/db')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Student = require('./routes/student');
+const Test = require('./routes/test');
+const morgan = require('morgan');
+const db = require('./db/db');
 const path = require('path');
 
 app.use(bodyParser.json());
@@ -13,19 +13,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
-app.use('/student', Student)
-app.use('/test', Test)
+app.use('/student', Student);
+app.use('/test', Test);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-db.sync()
-  .then(() => app.listen(3000, function () {
-    console.log('Server is listening on port 3000!');
-    })
-  )
-  .catch(console.error);
+const init = async () => {
+  try {
+    await db.sync();
+    console.log('Database synced');
+
+    app.listen(3000, () => console.log('Server is listening on port 3000!'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+init();
