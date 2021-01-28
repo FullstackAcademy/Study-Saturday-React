b13a683d1a82fda28822611cca68cecf60d2b26a
Mary Gordanier <mcgordanier@gmail.com>
2021-01-28 14:01:12 -0500

    fix(models, routes): cleanup model associations & route comments

--- routes/test.js
+++ routes/test.js
@@ -2,45 +2,74 @@ const router = require('express').Router();
 const Test = require('../db/models/tests');
 const Student = require('../db/models/students');
 
+// GET /test
 router.get('/', async (req, res, next) => {
   try {
     const tests = await Test.findAll();
-    res.send(tests);
-  } catch (err) {
-    next(err);
+    res.json(tests);
+  } catch (error) {
+    next(error);
   }
 });
 
+// GET /test/passing
+router.get('/passing', async (req, res, next) => {
+  try {
+    const passingTests = await Test.getPassing();
+    res.json(passingTests);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /test/subjects/:subject
+router.get('/subjects/:subject', async (req, res, next) => {
+  try {
+    const testsBySubject = await Test.findAll({
+      where: {
+        subject: req.params.subject,
+      },
+    });
+    res.json(testsBySubject);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /test/:id
 router.get('/:id', async (req, res, next) => {
   try {
-    let test = await Test.findByPk(req.params.id);
-    if (test) {
-      res.send(test);
-    } else {
-      res.status(404).send('Test not found');
-    }
-  } catch (err) {
-    next(err);
+    const test = await Test.findByPk(req.params.id);
+    res.json(test);
+  } catch (error) {
+    next(error);
   }
 });
 
-router.post('/student/:studentId', async (req, res, next) => {
+// POST /test/students/:studentId
+router.post('/students/:studentId', async (req, res, next) => {
   try {
-    let student = await Student.findByPk(req.params.studentId);
+    const student = await Student.findByPk(req.params.studentId);
     let test = await Test.create(req.body);
-    let studentTest = await test.setStudent(student);
-    res.status(201).send(studentTest);
-  } catch (err) {
-    next(err);
+    test = await test.setStudent(student);
+    res.status(201).json(test);
+  } catch (error) {
+    next(error);
   }
 });
 
+// DELETE /test/:id
 router.delete('/:id', async (req, res, next) => {
   try {
-    await Test.destroy({ where: { id: req.params.id } });
-    res.status(204).send();
-  } catch (err) {
-    next(err);
+    const test = await Test.findByPk(req.params.id);
+    if (test) {
+      await test.destroy();
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
