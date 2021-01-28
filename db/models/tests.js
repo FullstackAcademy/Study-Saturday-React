a6c65d0a14b68169ab07a3d2ae1b77c3910cd3c7
Mary Gordanier <mcgordanier@gmail.com>
2021-01-28 14:03:12 -0500

    fix(models, routes): cleanup model associations & route comments

--- db/models/tests.js
+++ db/models/tests.js
@@ -15,7 +15,7 @@ const Test = db.define('test', {
   },
 });
 
-Test.passing = function() {
+Test.passing = function () {
   return Test.findAll({
     where: {
       grade: {
@@ -25,7 +25,7 @@ Test.passing = function() {
   });
 };
 
-Test.findBySubject = function(type) {
+Test.findBySubject = function (type) {
   return Test.findAll({
     where: {
       subject: type,
@@ -33,7 +33,7 @@ Test.findBySubject = function(type) {
   });
 };
 
-Test.belongsTo(Student, { as: 'student' });
+Test.belongsTo(Student);
 Student.hasMany(Test);
 
 module.exports = Test;
