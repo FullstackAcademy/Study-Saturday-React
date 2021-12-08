6d94567e69002e89e3be34a1ed37da148a714c68
Andrea Childress <aesselman0830@gmail.com>
2021-12-08 14:14:40 -0500

    Andrea's update to Cycle 2 Hooks

--- browser/components/Main.js
+++ browser/components/Main.js
@@ -1,47 +1,86 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: [],
-    };
-  }
+export default function Main(props){
+  const [students, setStudents] = useState([]);
 
-  componentDidMount() {
-    this.getStudents();
-  }
-
-  async getStudents() {
+  const getStudents = async () => {
     console.log('fetching');
     try {
       const { data } = await axios.get('/student');
-      this.setState({ students: data });
+      setStudents(data);
     } catch (err) {
       console.error(err);
     }
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Students</h1>
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-            </tr>
-            {this.state.students.map(student => {
-              return (
-                <tr key={student.id}>
-                  <td>{student.fullName}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  useEffect(()=> {
+      getStudents()
+    }, [students]
+  )
+
+  return (
+    <div>
+      <h1>Students</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+          </tr>
+          {students.map(student => {
+            return (
+              <tr key={student.id}>
+                <td>{student.fullName}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
+// export default class Main extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       students: [],
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.getStudents();
+//   }
+
+//   async getStudents() {
+//     console.log('fetching');
+//     try {
+//       const { data } = await axios.get('/student');
+//       this.setState({ students: data });
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Students</h1>
+//         <table>
+//           <tbody>
+//             <tr>
+//               <th>Name</th>
+//             </tr>
+//             {this.state.students.map(student => {
+//               return (
+//                 <tr key={student.id}>
+//                   <td>{student.fullName}</td>
+//                 </tr>
+//               );
+//             })}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
